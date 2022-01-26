cd9f6503797e232b7684ac2632f87efed633474e
danifj <fernandezjuarezd@gmail.com>
2022-01-26 10:53:23 -0300

    config css app.component
    Tuve que setear angular.json para habilitar css, ya que por defecto lo había puesto en scss. Además, agrego icono de mina usando material icon design, que en un principio probé instalar como componente, pero en realidad como import se puede usar en React. Acá puse el SVG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+
 import { Tabla } from './juego/tabla'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'buscaminassa';
   tabla = new Tabla(10, 5);
+
+  
+
 }
