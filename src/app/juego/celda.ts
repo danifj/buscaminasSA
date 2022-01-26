c5d1a18bd4c9af7b39031cd62d8a4f0b24fef338
danifj <fernandezjuarezd@gmail.com>
2022-01-25 23:38:50 -0300

    Verificar celdas y game over
    Se empieza a cambiar appcomponent, y se inicia lógica de verificación de celdas.

--- src/app/juego/celda.ts
+++ src/app/juego/celda.ts
@@ -1,4 +1,9 @@
 export class Celda {
     estado: 'abierto' | 'limpio' | 'bandera' = 'abierto';
     mina = false;
+    proximindadMinas = 0;
+
+    constructor(public fila: number, public columna: number) {
+
+    }
 }
--- src/app/juego/tabla.ts
+++ src/app/juego/tabla.ts
@@ -1,6 +1,6 @@
 import { Celda } from './celda';
 
-class Tabla {
+export class Tabla {
 
     celdas: Celda[][] = [];
     constructor(tamanio: number, minas: number) {
@@ -10,7 +10,7 @@ class Tabla {
             this.celdas[y] = [];
             for (let x = 0; x < tamanio; x++) {
 
-                this.celdas[y][x] = new Celda();
+                this.celdas[y][x] = new Celda(y, x);
 
             }
 
@@ -23,8 +23,23 @@ class Tabla {
     }
 
     generarCeldaAleatoria(): Celda {
+        
         const y = Math.floor(Math.random() * this.celdas.length);
         const x = Math.floor(Math.random() * this.celdas[y].length);
         return this.celdas[y][x];
+
     }
+
+    verificarCelda(celda: Celda){
+        if(celda.estado !== "abierto") {
+            return;
+        } else if (celda.mina) {
+            this.finJuego()
+        } else {
+
+        
+            celda.estado = celda.mina ?
+        }
+    }
+
 }
