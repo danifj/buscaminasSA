f338bfe3c19713597f9a576b2e00fff9aae0a63c
danifj <fernandezjuarezd@gmail.com>
2022-01-27 11:48:13 -0300

    Timer
    Timer pre desarrollado de otro proyecto que tenía. Sirvió para el caso. Algunos problemas para definir la pausa. En pruebas quedó OK.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,9 +20,12 @@ export class AppComponent {
   title = 'buscaminassa';
   tabla: Tabla
   displayedColumns: string[] = [''];
+  time: number = 0;
+  interval;
 
   constructor(public dialog: MatDialog) {
     this.reiniciar();
+    this.startTimer();
   }
 
   // manejarFinJuego(result: 'finjuego' | null){
@@ -64,15 +67,55 @@ export class AppComponent {
 
   reiniciar(){
     this.tabla = new Tabla(10, 10);
+    this.startTimer();
   }
 
   public openDialog(estado: string): void {
+
+    this.pauseTimer();
     const updateDialogRef = this.dialog.open(ResultadoComponent, {
       data: {
         estadoJuego: estado,
       },
     });
+    
   }
 
+  transform(value: number, args?: any): string {
+
+    const hours: number = Math.floor(value / 60);
+    const minutes: number = (value - hours * 60);
+
+    if (hours < 10 && minutes < 10) {
+        return '0' + hours + ' : 0' + (value - hours * 60);
+    }
+    if (hours > 10 && minutes >= 10) {
+        return '0' + hours + ' : ' + (value - hours * 60);
+    }
+    if (hours > 10 && minutes < 10) {
+        return hours + ' : 0' + (value - hours * 60);
+    }
+    if (minutes >= 10) {
+        return '0' + hours + ' : ' + (value - hours * 60);
+    }
+  }
+
+  startTimer() {
+    this.time = 0;
+    clearInterval(this.interval);
+    console.log("=====>");
+    this.interval = setInterval(() => {
+      if (this.time === 0) {
+        this.time++;
+      } else {
+        this.time++;
+      }
+    }, 1000);
+  }
+
+  pauseTimer() {
+    this.time = this.time;
+    clearInterval(this.interval);
+  }
 
 }
